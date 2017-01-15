931fff3983f75cef329bf6ba7b3080a3d640600b
mlimberg <limbergmike@gmail.com>
2017-01-15 13:31:15 -0700

    saving update

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router';
+
+export default class PhotoGrid extends Component {
+
+  render() {
+    return(
+      <div className='photo-grid'>
+      I'm the photo grid
+      </div>
+    )
+  }
+}
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router';
+
+export default class Single extends Component {
+
+  render() {
+    return(
+      <div className='single-photo'>
+        I'm the single photo
+      </div>
+    )
+  }
+}
